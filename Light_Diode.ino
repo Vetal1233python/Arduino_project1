117a8564ae371c7f2ab1553d58d2c7fa37bb0988
Vetal1233python <111809604+Vetal1233python@users.noreply.github.com>
2023-12-12 05:50:28 +0200

    Add files via upload

---
+++ Light_Diode.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(13,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(13,HIGH);
+  delay(5);
+  digitalWrite(13,LOW);
+  delay(3);
+  // put your main code here, to run repeatedly:
+
+}
